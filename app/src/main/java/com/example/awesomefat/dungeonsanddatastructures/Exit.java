c08574b9a327ffcf57ebafb7c20baed99e93cad8
Puncledorf <gagagagannondorf@gmail.com>
2018-04-05 07:59:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/awesomefat/dungeonsanddatastructures/Exit.java
@@ -0,0 +1,28 @@
+package com.example.awesomefat.dungeonsanddatastructures;
+
+/**
+ * Created by awesomefat on 3/29/18.
+ */
+
+public class Exit
+{
+    private Room r1;
+    private Room r2;
+
+    public Exit(Room r1, Room r2)
+    {
+        this.r1 = r1;
+        this.r2 = r2;
+    }
+
+    public void takeExit(Player p)
+    {
+        if (p.getCurrentRoom()==r1){
+            p.setCurrentRoom(r2);
+        }
+        else{
+            p.setCurrentRoom(r1);
+        }
+        //make the player move to the room they are NOT currently in.
+    }
+}
